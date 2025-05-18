efe801d69c135286eb4ca00c2b7aa9396bf7401c
MariiaAntoniuk <mariia.antonik18@gmail.com>
2025-05-18 17:36:00 +0300

    add files

---
+++ assets/js/main.js
@@ -0,0 +1,103 @@
+// Show menu
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+// Menu show
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+// Menu hidden
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+// Remove Menu Mobile
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// Swiper Home
+const swiperHome = new Swiper('.home__swiper', {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        renderBullet: (index, className) =>{
+           return '<span class="' + className + '">' + String(index + 1).padStart(2, '0') + "</span>";
+        },
+    },
+    // autoplay: {
+    //     delay: 5000,
+    // }
+});
+
+// Change Background Header
+const bgHeader = () =>{
+    const header = document.getElementById('header')
+
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', bgHeader)
+
+// Show Scroll Up
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+// Scroll Sections Active Link
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id') ,
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId +']')
+
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+// Scroll Reveal Animation
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 300,
+    // reset: true, // Animations repeat
+})
+
+sr.reveal(`.home__bg`, {scale: 1.1, opacity: 1})
+sr.reveal(`.home__swiper`, {origin: 'right', distance: '300px', delay: 800})
+sr.reveal(`.home__data`, {origin: 'bottom', distance: '120px', delay: 1600})
+sr.reveal(`.swiper-pagination-bullet`, {origin: 'top', delay: 1800, opacity: 0})
+sr.reveal(`.home__button`, {origin: 'top', delay: 2200})
+sr.reveal(`.about__data, .contact__content`, {origin: 'left'})
+sr.reveal(`.about__video, .contact__img`, {origin: 'right'})
+sr.reveal(`.models__card`, {interval: 100})
+sr.reveal(`.info__img`, {distance: '120px'})
+sr.reveal(`.info__number`, {origin: 'bottom', distance: '80px', delay: 800})
+sr.reveal(`.info__group`, {interval: 100, delay: 1300})
+sr.reveal(`.footer__container`)
